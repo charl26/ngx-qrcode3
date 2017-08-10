d8b70df67ee2275e7569f46c24a6495ac31ee27b
Javier Sánchez <javiershoz@gmail.com>
2017-08-10 15:57:11 +0200

    Update ngx-qrcode.component.ts

--- src/ngx-qrcode.component.ts
+++ src/ngx-qrcode.component.ts
@@ -12,7 +12,7 @@ let QRCode = require('qrcode');
 export class NgxQRCodeComponent implements OnChanges {
 
   @Input('qrc-element-type') elementType: 'url' | 'img' | 'canvas' = 'url';
-  @Input('qrc-element-type') version: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40'= 'url';
+  @Input('qrc-element-type') version: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40'= '';
   @Input('qrc-element-type') correctionLevel: 'L' | 'M' | 'Q' | 'H' = 'H';
   @Input('qrc-class') cssClass = 'qrcode'; 
   @Input('qrc-value') value = 'Techiediaries';
