af4d36e9f660e34fbe7d0eb0ed23af3a919f5648
Javier Sánchez <javiershoz@gmail.com>
2017-08-10 15:09:27 +0200

    Update ngx-qrcode.component.ts
    Added functionality to include custom correction level and version.

--- src/ngx-qrcode.component.ts
+++ src/ngx-qrcode.component.ts
@@ -12,6 +12,8 @@ let QRCode = require('qrcode');
 export class NgxQRCodeComponent implements OnChanges {
 
   @Input('qrc-element-type') elementType: 'url' | 'img' | 'canvas' = 'url';
+  @Input('qrc-element-type') version: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40'= 'url';
+  @Input('qrc-element-type') correctionLevel: 'L' | 'M' | 'Q' | 'H' = 'H';
   @Input('qrc-class') cssClass = 'qrcode'; 
   @Input('qrc-value') value = 'Techiediaries';
   @ViewChild('qrcElement') qrcElement: ElementRef;
@@ -40,7 +42,7 @@ export class NgxQRCodeComponent implements OnChanges {
   }
   toCanvas(canvas){
     return new Promise((resolve,reject)=>{
-      QRCode.toCanvas(canvas, this.value,{ errorCorrectionLevel: 'H' } ,function (error) {
+      QRCode.toCanvas(canvas, this.value,{ errorCorrectionLevel: this.correctionLevel, version: this.version } ,function (error) {
         if (error){
           //console.error(error);
           reject(error);
